9272cf6318fa3e1eceb78580efcc68763f64bc41
nithinpanjala <nithin15chinnu@gmail.com>
2022-06-07 17:49:00 +0530

    final commit on 7th june

--- src/main/java/com/foodmunch/Entity/Cart.java
+++ src/main/java/com/foodmunch/Entity/Cart.java
@@ -1,5 +1,7 @@
 package com.foodmunch.Entity;
 
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,8 +9,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 
 @Entity
@@ -25,9 +30,63 @@ public class Cart {
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "custAddressId", referencedColumnName = "custAddressId")
 	private CustomerAddress deliveryAddress;
+	
+	@JsonManagedReference(value = "orderTable")
+	@OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+	private Set<OrderItems> orderItems;
 
-	@OneToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name = "orderTableId", referencedColumnName = "orderTableId")
-	private OrderTable orderTable;
+	public Cart() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Cart(int cartNo, float totalPrice, CustomerAddress deliveryAddress, Set<OrderItems> orderItems) {
+		super();
+		this.cartNo = cartNo;
+		this.totalPrice = totalPrice;
+		this.deliveryAddress = deliveryAddress;
+		this.orderItems = orderItems;
+	}
+
+	public Cart(int cartNo, float totalPrice, CustomerAddress deliveryAddress) {
+		super();
+		this.cartNo = cartNo;
+		this.totalPrice = totalPrice;
+		this.deliveryAddress = deliveryAddress;
+	}
+
+	public int getCartNo() {
+		return cartNo;
+	}
+
+	public void setCartNo(int cartNo) {
+		this.cartNo = cartNo;
+	}
+
+	public float getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(float totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public CustomerAddress getDeliveryAddress() {
+		return deliveryAddress;
+	}
+
+	public void setDeliveryAddress(CustomerAddress deliveryAddress) {
+		this.deliveryAddress = deliveryAddress;
+	}
+
+	public Set<OrderItems> getOrderItems() {
+		return orderItems;
+	}
+
+	public void setOrderItems(Set<OrderItems> orderItems) {
+		this.orderItems = orderItems;
+	}
+
+	
 
 }
---
+++ src/main/java/com/foodmunch/Entity/OrderItems.java
@@ -0,0 +1,105 @@
+package com.foodmunch.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+@Table(name = "OrderItems")
+public class OrderItems {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int OrderItemsId;
+	private long OrderCustId;
+	private int orderRestId;
+	private int orderFoodId;
+	private int quantity;
+	
+		@ManyToOne
+		@JsonBackReference(value = "orderTable")
+		@JoinColumn(name = "cartNo",referencedColumnName = "cartNo" )
+		private Cart cart;
+
+		public OrderItems() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+
+		public OrderItems(int orderItemsId, long orderCustId, int orderRestId, int orderFoodId, int quantity,
+				Cart cart) {
+			super();
+			OrderItemsId = orderItemsId;
+			OrderCustId = orderCustId;
+			this.orderRestId = orderRestId;
+			this.orderFoodId = orderFoodId;
+			this.quantity = quantity;
+			this.cart = cart;
+		}
+
+		public OrderItems(int orderItemsId, long orderCustId, int orderRestId, int orderFoodId, int quantity) {
+			super();
+			OrderItemsId = orderItemsId;
+			OrderCustId = orderCustId;
+			this.orderRestId = orderRestId;
+			this.orderFoodId = orderFoodId;
+			this.quantity = quantity;
+		}
+
+		public int getOrderItemsId() {
+			return OrderItemsId;
+		}
+
+		public void setOrderItemsId(int orderItemsId) {
+			OrderItemsId = orderItemsId;
+		}
+
+		public long getOrderCustId() {
+			return OrderCustId;
+		}
+
+		public void setOrderCustId(long orderCustId) {
+			OrderCustId = orderCustId;
+		}
+
+		public int getOrderRestId() {
+			return orderRestId;
+		}
+
+		public void setOrderRestId(int orderRestId) {
+			this.orderRestId = orderRestId;
+		}
+
+		public int getOrderFoodId() {
+			return orderFoodId;
+		}
+
+		public void setOrderFoodId(int orderFoodId) {
+			this.orderFoodId = orderFoodId;
+		}
+
+		public int getQuantity() {
+			return quantity;
+		}
+
+		public void setQuantity(int quantity) {
+			this.quantity = quantity;
+		}
+
+		public Cart getCart() {
+			return cart;
+		}
+
+		public void setCart(Cart cart) {
+			this.cart = cart;
+		}
+	
+	
+	
+	
+}
